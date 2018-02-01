856fc82a50f5111a10fcbc1b18f80db80faba6dd
Vetle Lode <vetlelode@gmail.com>
2018-02-01 12:14:05 +0100

    Update sql

--- sql/db.sql
+++ sql/db.sql
@@ -1,7 +1,12 @@
-CREATE TABLE text (
-    id serial PRIMARY KEY,
-    pubid integer NOT NULL,
-    text varchar NOT NULL,
-    title varchar,
-    created_at date
+CREATE TABLE public.text
+(
+  id integer NOT NULL DEFAULT nextval('text_id_seq'::regclass),
+  pubid integer NOT NULL,
+  text character varying NOT NULL,
+  created_at date,
+  title character varying,
+  CONSTRAINT text_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
 );
