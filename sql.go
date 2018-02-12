72a12fd168fe66fad53893cdec10aa83b463adbd
Vetle Lode <vetlelode@gmail.com>
2018-02-12 15:33:02 +0100

    Added delete post link to request to make my life easier for now

--- sql.go
+++ sql.go
@@ -143,29 +143,3 @@ func deletepost(post postdata) error {
 	db.Close()
 	return nil
 }
-
-/*
-Canned function for now because im bad at programming
-
-func findallposts() postcounter {
-	posts := postcounter{}
-	db := establishConn()
-	rows, err := db.Query("SELECT pubid, title FROM text")
-	if err != nil && err == sql.ErrNoRows {
-		log.Printf("No posts found most likely this is because the db is not properly setup/has no posts")
-		return posts
-	}
-	var derp int
-	var lul string
-	for rows.Next() {
-		err := rows.Scan(&derp, &lul)
-		if err != nil {
-			log.Fatal(err)
-		}
-		//posts = append(posts, postcounter{PostIDs: derp, Titles: lul})
-	}
-	log.Println(posts)
-	db.Close()
-	return posts
-}
-*/
