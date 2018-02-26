8d2824f7efa6ddd1fcf4be11f705cea8632f4865
Vetle Lode <vetlelode@gmail.com>
2018-02-26 15:02:46 +0100

    removed sanitization for now

--- markdown.go
+++ markdown.go
@@ -3,12 +3,11 @@ package main
 import (
 	"html/template"
 
-	"github.com/microcosm-cc/bluemonday"
 	"github.com/shurcooL/github_flavored_markdown"
 )
 
 func parse(input string) template.HTML {
-	input = bluemonday.UGCPolicy().Sanitize(input)
+	//input = bluemonday.UGCPolicy().Sanitize(input)
 	output := github_flavored_markdown.Markdown([]byte(input))
 	return template.HTML(string(output))
 }
