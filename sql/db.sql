adee2cfee629d8bbbf7538b66f6b544d39d8f7bc
vetlelode <vetlelode@gmail.com>
2018-04-13 15:55:33 +0200

    helg

--- sql/db.sql
+++ sql/db.sql
@@ -2,8 +2,9 @@ CREATE TABLE public.text
 (
    id character varying(100) PRIMARY KEY NOT NULL,
    editid character varying NOT NULL,
+   passforview boolean NOT NULL
    text character varying NOT NULL,
    created_at timestamp without time zone,
    title character varying,
-   views integer
+   views integer,
 );
