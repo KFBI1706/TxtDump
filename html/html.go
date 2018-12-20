c9c674afd192854b847bd3224906cd0922ad3e42
Vetle Lode <vetlelode@gmail.com>
2018-12-20 20:11:40 +0100

    Microcommit

--- html/html.go
+++ html/html.go
@@ -268,8 +268,7 @@ func Documentation(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err)
 	}
-	doc := parse(string(file))
-	err = tmpl.ExecuteTemplate(w, "doc", model.Markdown{MD: doc})
+	err = tmpl.ExecuteTemplate(w, "doc", model.Markdown{MD: parse(string(file))})
 	if err != nil {
 		log.Println(err)
 	}
